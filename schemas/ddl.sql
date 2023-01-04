fff7df4cf2447b6fb7a41e8c54fcb3f1c338914b
shin5ok <shin5ok@55mp.com>
2023-01-05 01:46:42 +0900

    copy ddl schema sql from sample

--- schemas/ddl.sql
+++ schemas/ddl.sql
@@ -1,27 +1,58 @@
-/*
-Reference from https://cloud.google.com/spanner/docs/schema-and-data-model
-*/
+-- Executing the schema creation in a batch will improve execution speed.
+start batch ddl;
 
-CREATE TABLE singers (
- singer_id   BIGINT PRIMARY KEY,
- first_name  VARCHAR(1024),
- last_name   VARCHAR(1024),
- singer_info BYTEA
- );
+create table if not exists singers (
+    id         varchar not null primary key,
+    first_name varchar,
+    last_name  varchar not null,
+    full_name  varchar generated always as (coalesce(concat(first_name, ' '::varchar, last_name), last_name)) stored,
+    active     boolean,
+    created_at timestamptz,
+    updated_at timestamptz
+);
 
-CREATE TABLE albums (
- singer_id     BIGINT,
- album_id      BIGINT,
- album_title   VARCHAR,
- PRIMARY KEY (singer_id, album_id)
- )
- INTERLEAVE IN PARENT singers ON DELETE CASCADE;
+create table if not exists albums (
+    id               varchar not null primary key,
+    title            varchar not null,
+    marketing_budget numeric,
+    release_date     date,
+    cover_picture    bytea,
+    singer_id        varchar not null,
+    created_at       timestamptz,
+    updated_at       timestamptz,
+    constraint fk_albums_singers foreign key (singer_id) references singers (id)
+);
 
-CREATE TABLE songs (
- singer_id     BIGINT,
- album_id      BIGINT,
- track_id      BIGINT,
- song_name     VARCHAR,
- PRIMARY KEY (singer_id, album_id, track_id)
- )
- INTERLEAVE IN PARENT albums ON DELETE CASCADE;
+create table if not exists tracks (
+    id           varchar not null,
+    track_number bigint not null,
+    title        varchar not null,
+    sample_rate  float8 not null,
+    created_at   timestamptz,
+    updated_at   timestamptz,
+    primary key (id, track_number)
+) interleave in parent albums on delete cascade;
+
+create table if not exists venues (
+    id          varchar not null primary key,
+    name        varchar not null,
+    description varchar not null,
+    created_at  timestamptz,
+    updated_at  timestamptz
+);
+
+create table if not exists concerts (
+    id          varchar not null primary key,
+    venue_id    varchar not null,
+    singer_id   varchar not null,
+    name        varchar not null,
+    start_time  timestamptz not null,
+    end_time    timestamptz not null,
+    created_at  timestamptz,
+    updated_at  timestamptz,
+    constraint fk_concerts_venues  foreign key (venue_id)  references venues  (id),
+    constraint fk_concerts_singers foreign key (singer_id) references singers (id),
+    constraint chk_end_time_after_start_time check (end_time > start_time)
+);
+
+run batch;
