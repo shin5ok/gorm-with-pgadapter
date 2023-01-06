52e84678f56da59395317d8122afcc5d874ab90e
shin5ok <shin5ok@55mp.com>
2023-01-06 22:21:27 +0900

    make mode to create album record

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/datatypes"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -19,6 +21,9 @@ func main() {
 	mode := flag.String("mode", "", "")
 	firstName := flag.String("firstname", "", "")
 	lastName := flag.String("lastname", "", "")
+	albumname := flag.String("albumname", "", "")
+	singerid := flag.String("singerid", "", "")
+
 	flag.Parse()
 
 	db, err := gorm.Open(postgres.Open(connString), &gorm.Config{
@@ -42,8 +47,17 @@ func main() {
 				return
 			}
 		}
+	case "createalbum":
+		if *singerid != "" && *albumname != "" {
+			_, err := CreateAlbum(db, *singerid, *albumname, 3)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
 	default:
-		fmt.Println("tako")
+		help()
+		return
 	}
 }
 
@@ -61,4 +75,39 @@ func CreateSinger(db *gorm.DB, firstName, lastName string) (string, error) {
 	}
 
 	return singer.ID, res.Error
+}
+
+func CreateAlbum(db *gorm.DB, singerId, albumTitle string, numTracks int) (string, error) {
+	albumId := uuid.NewString()
+	// We cannot include the Tracks that we want to create in the definition here, as gorm would then try to
+	// use an UPSERT to save-or-update the album that we are creating. Instead, we need to create the album first,
+	// and then create the tracks.
+	res := db.Create(&Album{
+		BaseModel: BaseModel{ID: albumId},
+		Title:     albumTitle,
+		// MarketingBudget: decimal.NullDecimal{Decimal: decimal.NewFromFloat(randFloat64(0, 10000000))},
+		ReleaseDate: datatypes.Date(time.Now()),
+		SingerId:    singerId,
+		// CoverPicture:    randBytes(randInt(5000, 15000)),
+	})
+	if res.Error != nil {
+		return albumId, res.Error
+	}
+	tracks := make([]*Track, numTracks)
+	for n := 0; n < numTracks; n++ {
+		randTrackTitle := fmt.Sprintf("track-%d", n)
+		tracks[n] = &Track{BaseModel: BaseModel{ID: albumId}, TrackNumber: int64(n + 1), Title: randTrackTitle}
+	}
+
+	// Note: The batch size is deliberately kept small here in order to prevent the statement from getting too big and
+	// exceeding the maximum number of parameters in a prepared statement. PGAdapter can currently handle at most 50
+	// parameters in a prepared statement.
+	res = db.CreateInBatches(tracks, 8)
+	log.Printf("%+v\n", tracks)
+
+	return albumId, res.Error
+}
+
+func help() {
+	fmt.Println("Nothing to do")
 }
