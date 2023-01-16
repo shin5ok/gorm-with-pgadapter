63e6d56a59d55e4012da3acb04d17e477e08663d
shin5ok <shin5ok@55mp.com>
2023-01-16 16:02:59 +0900

    adapt models for each schemas

--- db.go
+++ db.go
@@ -3,28 +3,33 @@ package main
 import (
 	"context"
 	"io"
+	"log"
 	"time"
 
-	"cloud.google.com/go/spanner"
 	"github.com/google/uuid"
-	"google.golang.org/api/iterator"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 type GameUserOperation interface {
-	createUser(context.Context, io.Writer, userParams) error
-	addItemToUser(context.Context, io.Writer, userParams, itemParams) error
+	createUser(context.Context, io.Writer, string) error
+	addItemToUser(context.Context, io.Writer, Users, itemParams) error
 	userItems(context.Context, io.Writer, string) ([]map[string]interface{}, error)
 }
 
-type userParams struct {
-	userID   string
-	userName string
+type BaseModel struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Users struct {
+	BaseModel
+	UserId string `gorm:"primaryKey;autoIncrement:false"`
+	Name   string
 }
 
 type itemParams struct {
+	BaseModel
 	itemID    string
 	itemPrice int
 }
@@ -41,11 +46,8 @@ func genId() string {
 func newClient(ctx context.Context, spannerString string) (dbClient, error) {
 
 	db, err := gorm.Open(postgres.Open(spannerString), &gorm.Config{
-		// DisableNestedTransaction will turn off the use of Savepoints if gorm
-		// detects a nested transaction. Cloud Spanner does not support Savepoints,
-		// so it is recommended to set this configuration option to true.
 		DisableNestedTransaction: true,
-		Logger:                   logger.Default.LogMode(logger.Error),
+		//Logger:                   logger.Default.LogMode(logger.Error),
 	})
 
 	if err != nil {
@@ -57,100 +59,33 @@ func newClient(ctx context.Context, spannerString string) (dbClient, error) {
 }
 
 // create a user
-func (d dbClient) createUser(ctx context.Context, w io.Writer, u userParams) error {
+func (d dbClient) createUser(ctx context.Context, w io.Writer, u string) error {
 
-	_, err := d.sc.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
-		sqlToUsers := `INSERT users (user_id, name, created_at, updated_at)
-		  VALUES (@userID, @userName, @timestamp, @timestamp)`
-		t := time.Now().Format("2006-01-02 15:04:05")
-		params := map[string]interface{}{
-			"userID":    u.userID,
-			"userName":  u.userName,
-			"timestamp": t,
-		}
-		stmtToUsers := spanner.Statement{
-			SQL:    sqlToUsers,
-			Params: params,
-		}
-		rowCountToUsers, err := txn.Update(ctx, stmtToUsers)
-		_ = rowCountToUsers
-		if err != nil {
-			return err
-		}
+	randomId := genId()
+	log.Printf("%+v\n", randomId)
 
-		return nil
-	})
-	return err
+	user := Users{
+		BaseModel: BaseModel{},
+		UserId:    randomId,
+		Name:      u,
+	}
+	log.Printf("%+v\n", user)
+	res := d.sc.Debug().Create(&user)
+	log.Printf("%+v\n", res)
+
+	if res.Error != nil {
+		return res.Error
+	}
+
+	return nil
 }
 
 // add item specified item_id to specific user
-func (d dbClient) addItemToUser(ctx context.Context, w io.Writer, u userParams, i itemParams) error {
-
-	_, err := d.sc.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
-		sqlToUsers := `INSERT user_items (user_id, item_id, created_at, updated_at)
-		  VALUES (@userID, @itemID, @timestamp, @timestamp)`
-		t := time.Now().Format("2006-01-02 15:04:05")
-		params := map[string]interface{}{
-			"userID":    u.userID,
-			"itemId":    i.itemID,
-			"timestamp": t,
-		}
-		stmtToUsers := spanner.Statement{
-			SQL:    sqlToUsers,
-			Params: params,
-		}
-		rowCountToUsers, err := txn.Update(ctx, stmtToUsers)
-		_ = rowCountToUsers
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-	return err
+func (d dbClient) addItemToUser(ctx context.Context, w io.Writer, u Users, i itemParams) error {
+	return nil
 }
 
 // get what items the user has
-func (d dbClient) userItems(ctx context.Context, w io.Writer, userID string) ([]map[string]interface{}, error) {
-
-	txn := d.sc.ReadOnlyTransaction()
-	defer txn.Close()
-	sql := `select users.name,items.item_name,user_items.item_id
-		from user_items join items on items.item_id = user_items.item_id join users on users.user_id = user_items.user_id
-		where user_items.user_id = @user_id`
-	stmt := spanner.Statement{
-		SQL: sql,
-		Params: map[string]interface{}{
-			"user_id": userID,
-		},
-	}
-
-	iter := txn.Query(ctx, stmt)
-	defer iter.Stop()
-
-	results := []map[string]interface{}{}
-	for {
-		row, err := iter.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			return results, err
-		}
-		var userName string
-		var itemNames string
-		var itemIds string
-		if err := row.Columns(&userName, &itemNames, &itemIds); err != nil {
-			return results, err
-		}
-
-		results = append(results,
-			map[string]interface{}{
-				"user_name": userName,
-				"item_name": itemNames,
-				"item_id":   itemIds,
-			})
-
-	}
-
-	return results, nil
+func (d dbClient) userItems(ctx context.Context, w io.Writer, userId string) ([]map[string]interface{}, error) {
+	return []map[string]interface{}{}, nil
 }
