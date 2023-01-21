de4bed3be95777993d19057dcdcb704f9c35eb15
shin5ok <shin5ok@55mp.com>
2023-01-21 18:06:01 +0900

    make userItems while modifying its signature

--- db.go
+++ db.go
@@ -14,7 +14,7 @@ import (
 type GameUserOperation interface {
 	createUser(context.Context, io.Writer, string) (string, error)
 	addItemToUser(context.Context, io.Writer, Users, ItemParams) error
-	userItems(context.Context, io.Writer, string) ([]map[string]interface{}, error)
+	userItems(context.Context, io.Writer, string) ([]ItemParams, error)
 }
 
 type BaseModel struct {
@@ -29,7 +29,8 @@ type Users struct {
 }
 
 type ItemParams struct {
-	ItemID string `gorm:"primaryKey;autoIncrement:false"`
+	ItemID   string `gorm:"primaryKey;autoIncrement:false"`
+	ItemName string
 }
 
 type userItems struct {
@@ -100,6 +101,32 @@ func (d dbClient) addItemToUser(ctx context.Context, w io.Writer, u Users, i Ite
 }
 
 // get what items the user has
-func (d dbClient) userItems(ctx context.Context, w io.Writer, userId string) ([]map[string]interface{}, error) {
-	return []map[string]interface{}{}, nil
+func (d dbClient) userItems(ctx context.Context, w io.Writer, userId string) ([]ItemParams, error) {
+	/*
+		sql := `select users.name,items.item_name,user_items.item_id
+			from user_items join items on items.item_id = user_items.item_id join users on users.user_id = user_items.user_id
+			where user_items.user_id = @user_id`
+	*/
+	rows, err := d.sc.Debug().Table("user_items").
+		Select("users.name as name,items.item_name as item_name,user_items.item_id as item_id, items.updated_at, items.created_at").
+		Joins("join items on items.item_id = user_items.item_id join users on users.user_id = user_items.user_id").
+		Where("user_items.user_id = ?", userId).
+		Rows()
+
+	if err != nil {
+		log.Printf("%+v", err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var r ItemParams
+	var resultUserItems []ItemParams
+
+	for rows.Next() {
+		d.sc.ScanRows(rows, &r)
+		resultUserItems = append(resultUserItems, r)
+	}
+
+	return resultUserItems, nil
 }
