936c5c85e9cbc15ebdb195b5e81623af9ae6c5b7
shin5ok <shin5ok@55mp.com>
2023-01-22 01:01:36 +0900

    for now add a test runs well, which is based on my another repo's one

---
+++ main_test.go
@@ -0,0 +1,147 @@
+/*
+This is just for local test with Spanner Emulator
+Note: Before running this test, run spanner emulator and create an instance as "test-instance"
+*/
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+
+	// "os"
+	// "path/filepath"
+	// "regexp"
+	"testing"
+	// "time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	fakeServing Serving
+
+	itemTestID = "d169f397-ba3f-413b-bc3c-a465576ef06e"
+	userTestID string
+)
+
+func init() {
+	db, err := gorm.Open(postgres.Open(spannerString), &gorm.Config{
+		DisableNestedTransaction: true,
+	})
+	_ = db
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fakeServing = Serving{
+		Client: dbClient{sc: db},
+	}
+	/*
+		schemaFiles, _ := filepath.Glob("schemas/*_ddl.sql")
+		if err := testutil.InitData(ctx, fakeDbString, schemaFiles); err != nil {
+			log.Fatal(err)
+		}
+	*/
+}
+
+func Test_run(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/", nil)
+	assert.Nil(t, err)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(fakeServing.pingPong)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Expected: %d. Got: %d, Message: %s", http.StatusOK, rr.Code, rr.Body)
+	}
+
+}
+
+func Test_createUser(t *testing.T) {
+
+	path := "test-user"
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("user_name", path)
+
+	r := &http.Request{}
+	req, err := http.NewRequestWithContext(r.Context(), "POST", "/api/user/"+path, nil)
+	assert.Nil(t, err)
+	newReq := req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(fakeServing.createUser)
+	handler.ServeHTTP(rr, newReq)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Expected: %d. Got: %d, Message: %s", http.StatusOK, rr.Code, rr.Body)
+	}
+	var u User
+	json.Unmarshal(rr.Body.Bytes(), &u)
+	userTestID = u.Id
+
+}
+
+// This test depends on Test_createUser
+func Test_addItemUser(t *testing.T) {
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("user_id", userTestID)
+	ctx.URLParams.Add("item_id", itemTestID)
+
+	r := &http.Request{}
+	uriPath := fmt.Sprintf("/api/user_id/%s/%s", userTestID, itemTestID)
+	req, err := http.NewRequestWithContext(r.Context(), "PUT", uriPath, nil)
+	t.Log(uriPath)
+	assert.Nil(t, err)
+	newReq := req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(fakeServing.addItemToUser)
+	handler.ServeHTTP(rr, newReq)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Expected: %d. Got: %d, Message: %s", http.StatusOK, rr.Code, rr.Body)
+	}
+
+}
+
+func Test_getUserItems(t *testing.T) {
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("user_id", userTestID)
+
+	r := &http.Request{}
+	uriPath := fmt.Sprintf("/api/user_id/%s", userTestID)
+	req, err := http.NewRequestWithContext(r.Context(), "GET", uriPath, nil)
+	assert.Nil(t, err)
+	newReq := req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(fakeServing.addItemToUser)
+	handler.ServeHTTP(rr, newReq)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Expected: %d. Got: %d, Message: %s", http.StatusOK, rr.Code, rr.Body)
+	}
+}
+
+/*
+func Test_cleaning(t *testing.T) {
+	t.Cleanup(
+		func() {
+			ctx := context.Background()
+			if err := testutil.DropData(ctx, fakeDbString); err != nil {
+				t.Error(err)
+			}
+		},
+	)
+} */
